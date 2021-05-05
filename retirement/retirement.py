3035ab88441be07ce85afe4794cef70369cb93e1
Will Martin <quarkswithforks@gmail.com>
2021-05-05 00:17:58 -0700

    cleaning up graphing WIP

--- retirement/retirement.py
+++ retirement/retirement.py
@@ -9,19 +9,38 @@ from matplotlib.widgets import Button
 
 class Manager:
     def __init__(self):
-        style.use('fivethirtyeight')
+        # style.use('fivethirtyeight')
         # Create a queue to store stock prices
         self.prices = []
         self.times = []
 
-        self.MAX_SAMPLES = 5
+        self.MAX_SAMPLES = 50
         self.UPDATE_INTERVAL = .5
-        self.FIRST_TIME = True
+        self.FIG_INITIALIZED = False
         self.data = {'x': [], 'y': [], 'x_p': [], 'y_p': []}
         self.ani = None
         self.ax1 = None
-        self.fig = plt.figure(figsize=(13, 6))
+        self.fig = plt.figure(figsize=(13, 9))
         self.button = None
+
+    def initialize_plot(self):
+        if self.FIG_INITIALIZED:
+            return
+
+        # this is the call to matplotlib that allows dynamic plotting
+        plt.ion()
+
+        # Create and label top graph (stock price)
+        plt.subplot(211)
+        plt.ylabel('Share Price')
+        plt.title('Etherium Share Price Over Time: {}'.format(''))
+
+        # Create and label bottom graph (rate of change)
+        plt.subplot(212)
+        plt.ylabel('Deriv Price')
+        plt.title('Deriv Share Price Over Time: {}'.format(''))
+
+        self.FIG_INITIALIZED = True
 
     def get_data(self):
         # Get the stock price
@@ -42,7 +61,7 @@ class Manager:
                 'x': self.times,
                 'y': self.prices
             }
-            self.data['y_p'] = list((1000 * (np.diff(self.data['y']) / np.diff(self.data['x']))))
+            self.data['y_p'] = list(np.diff(self.data['y']) / np.diff(self.data['x']))
             self.data['x_p'] = list((np.array(self.data['x'])[:-1] + np.array(self.data['x'])[1:]) / 2)
 
     @staticmethod
@@ -50,36 +69,45 @@ class Manager:
         print("Exiting")
         sys.exit(0)
 
-    def live_plotter(self, x_vec, y1_data, line1, subplot, identifier='', pause_time=0.1):
-        if x_vec != [] and len(x_vec) >= self.MAX_SAMPLES - 1:
-            if not line1:
-                # this is the call to matplotlib that allows dynamic plotting
-                plt.ion()
+    def live_plotter(self, x_data, y_data, this_plot, plot, pause_time=0.1):
+        if x_data != [] and len(x_data) >= self.MAX_SAMPLES - 1:
+            # Figure out which graph to update
+            if plot == 'price':
+                axis = plt.subplot(211)
+            else:
+                axis = plt.subplot(212)
 
-                ax = self.fig.add_subplot(subplot)
-                # create a variable for the line so we can later update it
-                line1, = ax.plot(x_vec, y1_data, '-o', alpha=0.8)
-                # update plot label/title
-                plt.ylabel('Share Price')
-                plt.title('Etherium Share Price Over Time: {}'.format(identifier))
+            if not this_plot:
+                if plot == 'price':
+                    this_plot, = axis.plot(x_data, y_data, '-o', alpha=0.8)
+                else:
+                    this_plot, = axis.plot(x_data, y_data, '-o', alpha=0.8)
 
-                # Put a stop button in place
-                button_pos = plt.axes([0.81, 0.05, 0.1, 0.075])
-                self.button = Button(button_pos, 'Stop')
-                self.button.on_clicked(self.stop_pressed)
+                return this_plot
 
-                plt.show()
+            self.initialize_plot()
 
             # after the figure, axis, and line are created, we only need to update the y-data
-            line1.set_ydata(y1_data)
+            this_plot.set_ydata(y_data)
+
             # adjust limits if new data goes beyond bounds
-            if np.min(y1_data) <= line1.axes.get_ylim()[0] or np.max(y1_data) >= line1.axes.get_ylim()[1]:
-                plt.ylim([np.min(y1_data) - np.std(y1_data), np.max(y1_data) + np.std(y1_data)])
+            current_min = np.min(y_data)
+            last_min = this_plot.axes.get_ylim()[0]
+            current_max = np.max(y_data)
+            last_max = this_plot.axes.get_ylim()[1]
+            if current_min <= last_min or current_max >= last_max:
+                axis.set_ylim([np.min(y_data) - np.std(y_data), np.max(y_data) + np.std(y_data)])
+
+            # Put a stop button in place
+            button_pos = plt.axes([0.81, 0.05, 0.1, 0.075])
+            self.button = Button(button_pos, 'Stop')
+            self.button.on_clicked(self.stop_pressed)
+
             # this pauses the data so the figure/axis can catch up - the amount of pause can be altered above
             plt.pause(pause_time)
 
         # return line so we can update it again in the next iteration
-        return line1
+        return this_plot
 
 
 def main():
@@ -93,8 +121,8 @@ def main():
     while True:
         manager.get_data()
 
-        share_plot = manager.live_plotter(manager.data['x'], manager.data['y'], share_plot, 311)
-        deriv_plot = manager.live_plotter(manager.data['x_p'], manager.data['y_p'], deriv_plot, 313)
+        share_plot = manager.live_plotter(manager.data['x'], manager.data['y'], share_plot, 'price')
+        deriv_plot = manager.live_plotter(manager.data['x_p'], manager.data['y_p'], deriv_plot, 'deriv')
 
         time.sleep(manager.UPDATE_INTERVAL)
 
